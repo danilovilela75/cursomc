06ba1bf31fb0c0981ec66d44a614b1272d699e06
danilovilela75 <danilovilela@teleset.com.br>
2020-05-07 16:07:35 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/teleset/services/ClienteService.java
@@ -0,0 +1,30 @@
+package br.com.teleset.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.teleset.domain.Cliente;
+import br.com.teleset.repositories.ClienteRepository;
+import br.com.teleset.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: "
+				+ id
+				+ ", Tipo: "
+				+ Cliente.class.getName()));
+		
+	}
+
+}
