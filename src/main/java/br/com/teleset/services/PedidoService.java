77c998af514adc821be0cedef0c7ae8b2956d58f
danilovilela75 <danilovilela@teleset.com.br>
2020-05-10 20:20:34 -0300

    endpoint /pedidos/id disponibilizado

---
+++ src/main/java/br/com/teleset/services/PedidoService.java
@@ -0,0 +1,30 @@
+package br.com.teleset.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.teleset.domain.Pedido;
+import br.com.teleset.repositories.PedidoRepository;
+import br.com.teleset.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		
+		Optional<Pedido> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: "
+				+ id
+				+ ", Tipo: "
+				+ Pedido.class.getName()));
+		
+	}
+
+}
