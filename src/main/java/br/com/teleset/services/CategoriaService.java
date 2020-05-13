dcb10c9d13c26d7f0844342d4f53f097ed7180a3
danilovilela75 <danilovilela@teleset.com.br>
2020-05-13 14:55:12 -0300

    atualizando com put

--- src/main/java/br/com/teleset/services/CategoriaService.java
+++ src/main/java/br/com/teleset/services/CategoriaService.java
@@ -32,5 +32,11 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
+	
+	//update
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
 
 }
