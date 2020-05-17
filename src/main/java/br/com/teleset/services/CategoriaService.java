925c436150de80989047fd10bb0020d828137589
danilovilela75 <danilovilela@teleset.com.br>
2020-05-17 20:03:57 -0300

    validacao sintatica com beam validation

--- src/main/java/br/com/teleset/services/CategoriaService.java
+++ src/main/java/br/com/teleset/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.teleset.domain.Categoria;
+import br.com.teleset.dto.CategoriaDTO;
 import br.com.teleset.repositories.CategoriaRepository;
 import br.com.teleset.services.exceptions.ObjectNotFoundException;
 
@@ -64,6 +65,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
 }
