4a52c909f4c3e51727ad30275d429ce5f89e1162
danilovilela75 <danilovilela@teleset.com.br>
2020-05-26 23:17:28 -0300

    CRUD clientes completo e corrigido

--- src/main/java/br/com/teleset/resources/ClienteResource.java
+++ src/main/java/br/com/teleset/resources/ClienteResource.java
@@ -1,13 +1,24 @@
 package br.com.teleset.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.teleset.domain.Cliente;
+import br.com.teleset.dto.ClienteDTO;
 import br.com.teleset.services.ClienteService;
 
 @RestController
@@ -24,6 +35,57 @@ public class ClienteResource {
 		
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	//inserir Cliente
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				  .path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	//ALTERAR CLIENTE
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	//delete cliente
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	//visualizar todos os clientes
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	//paginacao clientes
+	@RequestMapping(value = "/pages", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0")
+			Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24")
+			Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome")
+			String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC")
+			String direction
+			) {
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
--- src/main/java/br/com/teleset/services/ClienteService.java
+++ src/main/java/br/com/teleset/services/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.teleset.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.teleset.domain.Cliente;
+import br.com.teleset.dto.ClienteDTO;
 import br.com.teleset.repositories.ClienteRepository;
+import br.com.teleset.services.exceptions.DataIntegrityException;
 import br.com.teleset.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -25,6 +32,45 @@ public class ClienteService {
 				+ ", Tipo: "
 				+ Cliente.class.getName()));
 		
+	}
+
+	//inserir cliente
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	//atualizar cliente
+	public Cliente update(Cliente obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	//delete cliente
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Proibido excluir com pedidos ativos!");
+		}
+	}
+	
+	//listar todos os clientes
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	//paginar clientes
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	//fromDTO
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
 	}
 
 }
